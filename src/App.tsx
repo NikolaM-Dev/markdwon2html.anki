2f383bbc44c7e7bde8677c8dc36ea5112f49b61b
Juan David Merchan Torres <juan1014166@gmail.com>
2025-02-24 14:48:16 -0500

    feat(src): Add base mvp

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Converter } from 'showdown';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showNotify, setShowNotify] = useState<boolean>(false);
+
+  const converter = new Converter();
+
+  const onCopy = async () => {
+    setShowNotify(true);
+
+    const clipboardContents = await navigator.clipboard.readText();
+    const convertedHTML = converter.makeHtml(clipboardContents);
+    const formattedHTML = convertedHTML.replace(
+      '<p><strong>Examples:</strong></p>',
+      '<br>\n<p><strong>Examples:</strong></p>'
+    );
+
+    await navigator.clipboard.writeText(formattedHTML);
+
+    const target = document.getElementById('target')!;
+    target.innerHTML = convertedHTML;
+
+    setTimeout(() => {
+      setShowNotify(false);
+    }, 500);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <div id="target"></div>
+      <button onClick={onCopy}>Copy</button>
+      {showNotify ? <p>✅ Copied...</p> : null}
+    </main>
+  );
 }
 
-export default App
+export default App;
